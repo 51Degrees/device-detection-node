8400a262bc88a2c1bdde24c821deb348c0f13fb7
Piotr Grabalski <piotr@51degrees.com>
2021-06-15 09:21:23 +0000

    Merged PR 3956: UACH from rebased feature/UACH-4.3.0
    - Merged PR 3614: Fixed warnings generated by C/C++ compilation process and treat all warnings as errors for Windows and MacOS platforms
    - DOC: Replace www.51degrees.com with 51degrees.com
    - EXAMPLE: Added example to manually set the User Agent Client Hints using SetHeader properties.
    - Updated datafile path in the example.
    - REF: Update reference package version
    - FEAT:  Property tests for device detection cloud and on-premise
    - BUILD: Use ubuntu-18.04 for gitversion support
    - EXAMPLE: Updated example to match core changes to client-hints.
    - EXAMPLE: Updated examples to match core changes to client-hints.
    - EXAMPLE: Improved explanation of additional response headers.
    - REORG: Updated usage of response headers to match changes in core.
    - BUILD: Add the pseudoheader.c file to the build process.
    - EXAMPLE: Update the UACH example to bring it in line with other languages.
    - EXAMPLE: Add a new example to demonstrate detection using user-agent client hints.
    - GIT: Updated reference to device-detection-cxx submodule.
    - TEST: updated useragentclienthints tests.
    - REF: Update to latest device-detection-cxx. Also correct a few issues with output from the user agent client hints example.
    - VER: Update remote_package.json file to use correct package version
    - BUG/EXAMPLE: Incorrect element was used to get the no value message.
    - EXAMPLE: Fixed incorrect snippet and example paths.
    - BUILD: User super resource key for build-and-test CI
    - Merge branch 'feature/UACH-4.3.0' of https://51degrees.visualstudio.com/DefaultCollection/Pipeline/_git/device-detection-node into feature/UACH-4.3.0

--- fiftyone.devicedetection.onpremise/examples/hash/matchMetrics.js
+++ fiftyone.devicedetection.onpremise/examples/hash/matchMetrics.js
@@ -25,7 +25,7 @@
 
 @include{doc} example-match-metrics-hash.txt
 
-This example is available in full on [GitHub](https://github.com/51Degrees/device-detection-node/blob/master/fiftyone.devicedetection/examples/hash/matchMetrics.js).
+This example is available in full on [GitHub](https://github.com/51Degrees/device-detection-node/blob/master/fiftyone.devicedetection.hash/examples/hash/matchMetrics.js).
 
 @include{doc} example-require-datafile.txt
 
