5de448d2e5577c438df70b83db300622bed3651c
romansertsov <roman.sertsov@postindustria.com>
2023-06-26 18:09:44 +0300

    Params added to performance.js

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -5,23 +5,24 @@ param (
     [string]$Name
 )
 
-$perfSummary = New-Item -ItemType directory -Path $RepoName/test-results/performance-summary -Force
-
 Push-Location $RepoName
 try
 {
     Write-Output "Running performance tests"
     $env:JEST_JUNIT_OUTPUT_DIR = 'test-results/performance'
 
-    # Used for generating performance output
-    $env:PERFORMANCE_JSON_OUTPUT = 'true'
+    $perfSummary = New-Item -ItemType directory -Path $RepoName/test-results/performance-summary -Force
+    $perfJSONOutputName = "results_$Name.json";
+
+    Start-Process -FilePath "node" -ArgumentList "$RepoName/examples/onpremse/performance-console/performance.js", "--jsonoutput", $perfJSONOutputName -Wait
+
     npm run performance-test || $($testsFailed = $true)
 
-    Get-Content -Path performance_test_summary.json
+    Get-Content -Path $perfJSONOutputName
 
     Write-Output "Path to performance results - $perfSummary/results_$Name.json"
 
-    Move-Item -Path performance_test_summary.json -Destination $perfSummary/results_$Name.json || $(throw "failed to move summary")
+    Move-Item -Path $perfJSONOutputName -Destination $perfSummary || $(throw "failed to move summary")
     Write-Output "OK"
 
 } finally {
