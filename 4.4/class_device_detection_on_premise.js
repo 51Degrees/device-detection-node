669527667a46133bacd3cf1aebe6232779c4d516
Automation51D <51DCI@51Degrees.com>
2023-10-07 03:31:11 +0000

    REF: Updated documentation.

--- 4.4/class_device_detection_on_premise.js
+++ 4.4/class_device_detection_on_premise.js
@@ -1,6 +1,6 @@
 var class_device_detection_on_premise =
 [
-    [ "constructor", "class_device_detection_on_premise.html#ad36104ea3f59adea7a5cdf34490973de", null ],
+    [ "constructor", "class_device_detection_on_premise.html#a69b986acb5a6a98acc739dd22c971571", null ],
     [ "processInternal", "class_device_detection_on_premise.html#acc5c63c1c0497faed9951cad851c2eda", null ],
     [ "profiles", "class_device_detection_on_premise.html#a70c99c4f2e5a3289a2c71d217e48d4ad", null ]
 ];
--- 4.4/class_device_detection_on_premise_pipeline_builder.js
+++ 4.4/class_device_detection_on_premise_pipeline_builder.js
@@ -1,4 +1,4 @@
 var class_device_detection_on_premise_pipeline_builder =
 [
-    [ "constructor", "class_device_detection_on_premise_pipeline_builder.html#acb81e4d3cee71ada22e2ed94ab6bbb76", null ]
+    [ "constructor", "class_device_detection_on_premise_pipeline_builder.html#ae936076aacc3298e68ddc8c2a386b443", null ]
 ];
