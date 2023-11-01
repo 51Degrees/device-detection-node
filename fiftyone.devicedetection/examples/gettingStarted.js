52a629cddb2850ef25cc4379f6b0202dd93cddca
James Rogers <JamesR@51degrees.com>
2023-11-01 11:29:53 +0000

    update links

--- fiftyone.devicedetection/examples/gettingStarted.js
+++ fiftyone.devicedetection/examples/gettingStarted.js
@@ -28,7 +28,7 @@
 This shows how to use DeviceDetectionPipelineBuilder instead of specific
 DeviceDetectionCloudPipelineBuilder.
 
-This example is available in full on [GitHub](https://github.com/51Degrees/device-detection-node/blob/master/fiftyone.devicedetection/examples/gettingStarted.js).
+This example is available in full on [GitHub](https://github.com/51Degrees/device-detection-node/blob/main/fiftyone.devicedetection/examples/gettingStarted.js).
 
 @include{doc} example-require-resourcekey.txt
 
