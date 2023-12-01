e0020b116a4eee55fe7cef71782aa6abdcceaa0d
Automation51D <51DCI@51Degrees.com>
2023-12-01 03:31:27 +0000

    REF: Updated documentation.

--- 4.4/class_device_detection_data_file.js
+++ 4.4/class_device_detection_data_file.js
@@ -1,5 +1,5 @@
 var class_device_detection_data_file =
 [
-    [ "constructor", "class_device_detection_data_file.html#ac97dc4f3202d18b9bbb10937f374e7e7", null ],
+    [ "constructor", "class_device_detection_data_file.html#a46d2a5ef2d2a89ed5b5329e1864be399", null ],
     [ "urlFormatter", "class_device_detection_data_file.html#a88d3006826d161ec65892a342f82b633", null ]
 ];
