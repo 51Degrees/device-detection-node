adc5a3975880a4038398fb9be8bd2e3d46941d75
romansertsov <roman.sertsov@postindustria.com>
2024-03-18 14:44:41 +0200

    path to data file fix

--- fiftyone.devicedetection.onpremise/tests/deviceDetectionOnPremise.test.js
+++ fiftyone.devicedetection.onpremise/tests/deviceDetectionOnPremise.test.js
@@ -38,7 +38,7 @@ const fs = require('fs');
 const zlib = require('zlib');
 let server;
 
-const DataFile = path.resolve((process.env.directory || __dirname) + '/../device-detection-cxx/device-detection-data/51Degrees-LiteV4.1.hash');
+const DataFile = path.resolve((process.env.directory || __dirname) + '../device-detection-cxx/device-detection-data/51Degrees-LiteV4.1.hash');
 
 describe('deviceDetectionOnPremise', () => {
   // Check that an exception is thrown if license key is not
